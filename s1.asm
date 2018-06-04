ec4feaa01700f2b013038b7a9be1ff04cd194d7d
markwinds <38316819+markwinds@users.noreply.github.com>
2018-06-04 16:37:42 +0800

    完成输入密码，未加密

--- s1.asm
+++ s1.asm
@@ -1,8 +1,9 @@
+;密码未加密
 org 7c00h
 
 jmp uboot
 
-keyin 		db 	'00000000000000000000000000000000000'	;用来存放输入内容的空间
+keyin 		db 	'000000000000000000000000000000000000000'	;用来存放输入内容的空间
 hint		db 	'Please put in you personal password.#'
 errors 		db 	'error!#'
 welcomes 	db 	'You are welcome Mark!#'
@@ -27,12 +28,12 @@ string:
 	ret
 
 
-error:
-	mov di,errors		
-	mov si,160d			
-	mov bl,0x0c			
-	call string		
-	jmp $	
+character:
+	mov byte[es:si],bl		;将显示的内容放在bl中
+	inc si
+	mov byte[es:si],0x09	;颜色默认是淡蓝色
+	inc si
+	ret
 
 
 input:
@@ -44,15 +45,17 @@ input:
 	je inputend
 	mov byte[ds:di],al
 	inc di
-	mov byte[es:si],al
-	inc si
-	mov byte[es:si],bl
-	inc si
+	mov bl,'*'
+	call character
 	jmp inputbegin
 
 	inputend:
-	; mov byte[ds:di],al
 	ret
+
+
+
+
+
 
 uboot:
 	;默认进入80*25的文本模式，起始地址0xB8000
@@ -63,44 +66,23 @@ uboot:
 	mov ax,0
 	mov ds,ax
 
+
 	mov di,hint			;确定要显示的字符串
 	mov si,0x00			;在第一行第一位显示字符串1
-	mov bl,0x0c			;字符串1的颜色
+	mov bl,0x09			;字符串1的颜色
 	call string			;写入显存，显示字符串
-
-	; mov ah,0
-	; int 16h
-	; mov bx,keyin
-	; mov byte[ds:bx],al
-
-	; mov si,0x80			;在第一行第一位显示字符串1
-	; mov bl,0x0c			;字符串1的颜色
-	; mov byte[es:si],al
-	; inc si
-	; mov byte[es:si],bl
 
 
 	mov di,keyin			;存放输入数据的位置
 	mov si,0xa0				;显示输入的位置
-	mov bl,0x0c				;显示的颜色
 	call input
-	; inputbegin:
-	; mov ah,0
-	; int 16h
-	; cmp al,' '
-	; je inputend
-	; mov byte[ds:di],al
-	; inc di
-	; jmp inputbegin
-	; inputend:
-	; mov byte[ds:di],al
 	
 
-	mov si,secret		;cx指向密码
-	mov di,0xc0			;di指向输入的数据
+	mov si,secret			;si指向密码
+	mov di,keyin			;di指向输入的数据
 	mov bh,0x00
 	loop1:
-	cmp bh,0x10
+	cmp bh,0x0a
 	jnb welcome
 	mov al,[ds:si]
 	cmp al,[ds:di]
@@ -112,7 +94,15 @@ uboot:
 
 	welcome:
 	mov di,welcomes		
-	mov si,160d			
+	mov si,320d			
+	mov bl,0x09			
+	call string		
+	jmp $
+
+	error:
+	mov di,errors		
+	mov si,320d			
 	mov bl,0x0c			
 	call string		
+	jmp $	
 jmp $	
